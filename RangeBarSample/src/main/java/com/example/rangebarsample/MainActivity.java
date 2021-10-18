6f9066609b278b5385487536a08ac9b0ffc54980
Alex Kurop <alkurop@gmail.com>
2021-10-18 14:25:04 +0300

    Removed a bunch of crap

--- RangeBarSample/src/main/java/com/example/rangebarsample/MainActivity.java
+++ RangeBarSample/src/main/java/com/example/rangebarsample/MainActivity.java
@@ -1,14 +1,11 @@
 
 package com.example.rangebarsample;
 
-import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,55 +13,23 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.edmodo.rangebar.RangeBar;
 
-public class MainActivity extends Activity implements ColorPickerDialog.OnColorChangedListener {
-
-    // Corresponds to Color.LTGRAY
-    private static final int DEFAULT_BAR_COLOR = 0xffcccccc;
-
-    // Corresponds to android.R.color.holo_blue_light.
-    private static final int DEFAULT_CONNECTING_LINE_COLOR = 0xff33b5e5;
-    private static final int HOLO_BLUE = 0xff33b5e5;
+public class MainActivity extends AppCompatActivity {
 
     // Sets the initial values such that the image will be drawn
     private static final int DEFAULT_THUMB_COLOR_NORMAL = -1;
     private static final int DEFAULT_THUMB_COLOR_PRESSED = -1;
 
-    // Sets variables to save the colors of each attribute
-    private int mBarColor = DEFAULT_BAR_COLOR;
-    private int mConnectingLineColor = DEFAULT_CONNECTING_LINE_COLOR;
-    private int mThumbColorNormal = DEFAULT_THUMB_COLOR_NORMAL;
-    private int mThumbColorPressed = DEFAULT_THUMB_COLOR_PRESSED;
-
     // Initializes the RangeBar in the application
     private RangeBar rangebar;
-
-    // Saves the state upon rotating the screen/restarting the activity
-    @Override
-    protected void onSaveInstanceState(Bundle bundle) {
-        super.onSaveInstanceState(bundle);
-        bundle.putInt("BAR_COLOR", mBarColor);
-        bundle.putInt("CONNECTING_LINE_COLOR", mConnectingLineColor);
-        bundle.putInt("THUMB_COLOR_NORMAL", mThumbColorNormal);
-        bundle.putInt("THUMB_COLOR_PRESSED", mThumbColorPressed);
-    }
 
     // Restores the state upon rotating the screen/restarting the activity
     @Override
     protected void onRestoreInstanceState(Bundle bundle) {
         super.onRestoreInstanceState(bundle);
-        mBarColor = bundle.getInt("BAR_COLOR");
-        mConnectingLineColor = bundle.getInt("CONNECTING_LINE_COLOR");
-        mThumbColorNormal = bundle.getInt("THUMB_COLOR_NORMAL");
-        mThumbColorPressed = bundle.getInt("THUMB_COLOR_PRESSED");
-
-        // Change the text colors to the appropriate colors, and the text as
-        // well
-        colorChanged(Component.BAR_COLOR, mBarColor);
-        colorChanged(Component.CONNECTING_LINE_COLOR, mConnectingLineColor);
-        colorChanged(Component.THUMB_COLOR_NORMAL, mThumbColorNormal);
-        colorChanged(Component.THUMB_COLOR_PRESSED, mThumbColorPressed);
 
         // Gets the RangeBar
         rangebar = (RangeBar) findViewById(R.id.rangebar1);
@@ -95,17 +60,8 @@ public class MainActivity extends Activity implements ColorPickerDialog.OnColorC
         // Gets the buttons references for the buttons
         final Button barColor = (Button) findViewById(R.id.barColor);
         final Button connectingLineColor = (Button) findViewById(R.id.connectingLineColor);
-        final Button thumbColorNormal = (Button) findViewById(R.id.thumbColorNormal);
-        final Button thumbColorPressed = (Button) findViewById(R.id.thumbColorPressed);
-        final Button resetThumbColors = (Button) findViewById(R.id.resetThumbColors);
         final Button refreshButton = (Button) findViewById(R.id.refresh);
         
-        // Sets initial colors for the Color buttons
-        barColor.setTextColor(DEFAULT_BAR_COLOR);
-        connectingLineColor.setTextColor(DEFAULT_CONNECTING_LINE_COLOR);
-        thumbColorNormal.setTextColor(HOLO_BLUE);
-        thumbColorPressed.setTextColor(HOLO_BLUE);
-
         // Gets the RangeBar
         rangebar = (RangeBar) findViewById(R.id.rangebar1);
 
@@ -256,112 +212,11 @@ public class MainActivity extends Activity implements ColorPickerDialog.OnColorC
 
         // Setting Color Attributes---------------------------------
 
-        // Sets barColor
-        barColor.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                initColorPicker(Component.BAR_COLOR, mBarColor, mBarColor);
-            }
-        });
 
-        // Sets connectingLineColor
-        connectingLineColor.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                initColorPicker(Component.CONNECTING_LINE_COLOR, mConnectingLineColor, mConnectingLineColor);
-            }
-        });
-
-        // Sets thumbColorNormal
-        thumbColorNormal.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                initColorPicker(Component.THUMB_COLOR_NORMAL, mThumbColorNormal, mThumbColorNormal);
-            }
-        });
-
-        // Sets thumbColorPressed
-        thumbColorPressed.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                initColorPicker(Component.THUMB_COLOR_PRESSED, mThumbColorPressed, mThumbColorPressed);
-            }
-        });
-
-        // Resets the thumbColors if selected
-        resetThumbColors.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                rangebar.setThumbColorNormal(-1);
-                rangebar.setThumbColorPressed(-1);
-
-                mThumbColorNormal = -1;
-                mThumbColorPressed = -1;
-
-                thumbColorNormal.setText("thumbColorNormal = N/A");
-                thumbColorPressed.setText("thumbColorPressed = N/A");
-                thumbColorNormal.setTextColor(HOLO_BLUE);
-                thumbColorPressed.setTextColor(HOLO_BLUE);
-            }
-        });
 
     }
 
-    /**
-     * Sets the changed color using the ColorPickerDialog.
-     * 
-     * @param component Component specifying which input is being used
-     * @param newColor Integer specifying the new color to be selected.
-     */
-    @Override
-    public void colorChanged(Component component, int newColor) {
 
-        String hexColor = String.format("#%06X", (0xFFFFFF & newColor));
-
-        switch (component)
-        {
-            case BAR_COLOR:
-                mBarColor = newColor;
-                rangebar.setBarColor(newColor);
-                final TextView barColorText = (TextView) findViewById(R.id.barColor);
-                barColorText.setText("barColor = " + hexColor);
-                barColorText.setTextColor(newColor);
-                break;
-
-            case CONNECTING_LINE_COLOR:
-                mConnectingLineColor = newColor;
-                rangebar.setConnectingLineColor(newColor);
-                final TextView connectingLineColorText = (TextView) findViewById(R.id.connectingLineColor);
-                connectingLineColorText.setText("connectingLineColor = " + hexColor);
-                connectingLineColorText.setTextColor(newColor);
-                break;
-
-            case THUMB_COLOR_NORMAL:
-                mThumbColorNormal = newColor;
-                rangebar.setThumbColorNormal(newColor);
-                final TextView thumbColorNormalText = (TextView) findViewById(R.id.thumbColorNormal);
-
-                if (newColor == -1) {
-                    thumbColorNormalText.setText("thumbColorNormal = N/A");
-                    thumbColorNormalText.setTextColor(HOLO_BLUE);
-                }
-                else {
-                    thumbColorNormalText.setText("thumbColorNormal = " + hexColor);
-                    thumbColorNormalText.setTextColor(newColor);
-                }
-                break;
-
-            case THUMB_COLOR_PRESSED:
-                mThumbColorPressed = newColor;
-                rangebar.setThumbColorPressed(newColor);
-                final TextView thumbColorPressedText = (TextView) findViewById(R.id.thumbColorPressed);
-
-                if (newColor == -1) {
-                    thumbColorPressedText.setText("thumbColorPressed = N/A");
-                    thumbColorPressedText.setTextColor(HOLO_BLUE);
-                }
-                else {
-                    thumbColorPressedText.setText("thumbColorPressed = " + hexColor);
-                    thumbColorPressedText.setTextColor(newColor);
-                }
-        }
-    }
 
     /**
      * Sets the font on all TextViews in the ViewGroup. Searches recursively for
@@ -385,24 +240,6 @@ public class MainActivity extends Activity implements ColorPickerDialog.OnColorC
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-    /**
-     * Initiates the colorPicker from within a button function.
-     * 
-     * @param component Component specifying which input is being used
-     * @param initialColor Integer specifying the initial color choice. *
-     * @param defaultColor Integer specifying the default color choice.
-     */
-    private void initColorPicker(Component component, int initialColor, int defaultColor)
-    {
-        ColorPickerDialog colorPicker = new ColorPickerDialog(this,
-                                                              this,
-                                                              component,
-                                                              initialColor,
-                                                              defaultColor);
-        colorPicker.show();
-
     }
 
 }
